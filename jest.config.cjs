058aef9c2280df3223452890e5a5184aeaf1b83b
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-03-29 23:23:01 +0200

    fix: ignore test files when building

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,10 +1,24 @@
+// workaround until @swc/cli `--ignore` works, see https://github.com/swc-project/cli/issues/20
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const fs = require("fs");
+
+const swcConfig = JSON.parse(fs.readFileSync(`${__dirname}/.swcrc`));
+
 module.exports = {
   extensionsToTreatAsEsm: [".ts"],
   moduleNameMapper: {
     "^(\\.{1,2}/.*)\\.js$": "$1",
   },
   transform: {
-    "^.+\\.(t|j)s$": "@swc/jest",
+    "^.+\\.(t|j)s$": [
+      "@swc/jest",
+      {
+        ...swcConfig,
+        exclude: [],
+        swcrc: false,
+      },
+    ],
   },
   setupFiles: ["./jest.env.ts"],
   testEnvironment: "node",
--- jest.env.ts
+++ jest.env.ts
@@ -1,5 +1,2 @@
 import dotenv from "dotenv";
-
 dotenv.config({ path: ".env.test" });
-
-console.log("called", process.env);
