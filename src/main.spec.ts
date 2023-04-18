56162b42a60a6e8c9f11da4982544287c02842be
Felix Spöttel <1682504+fspoettel@users.noreply.github.com>
2023-04-18 11:33:32 +0200

    feat: add example test

---
+++ src/main.spec.ts
@@ -0,0 +1,7 @@
+import { main } from "./main.js";
+
+describe("main", () => {
+  it("does not throw an error", () => {
+    expect(main()).resolves.toBeTruthy();
+  });
+});
--- src/main.ts
+++ src/main.ts
@@ -1,2 +1,10 @@
 import dotenv from "dotenv";
 dotenv.config();
+
+// compiles to ESM, so top-level await works.
+export async function main() {
+  return true;
+}
+
+await main();
+console.log("hello world!");
